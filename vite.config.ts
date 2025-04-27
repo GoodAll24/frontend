3bf3835470f4f161aa7d6936c2a4bc9040519f16
GoodAll24 <alden.gonzalezp@gmail.com>
2025-04-26 22:22:07 -0400

    path configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    // en este alias se establece '@' como el directorio para ./src
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
